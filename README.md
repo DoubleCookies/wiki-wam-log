a0a8eca4ba179e5f5d4bedfd5297f02369e797c9
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2021-04-30 11:44:16 +0300

    layout updates (header, main, footer)

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Wiki WAM Log
-Small project for demonstration WAM score for some wikis I edited in 2016-2020. 
-This score from 0 to 100 (higher - better) represented different wiki metrics (exact combination of them is unknown).
+_(WAM = Wiki Activity Monitor)_
+
+This is small project for demonstration WAM score for some wikis I edited in 2016-2020. 
+WAM score ranges from 0 to 100 (higher - better) and represents different wiki metrics (exact combination of them is unknown).
 In 2021 WAM was removed from FANDOM wikis... and I guess it's OK.
 
 Created with [create-react-app](https://github.com/facebook/create-react-app).
 
 ## Launch
 In root folder you should type
-- `yarn install`  (for installing packages)
+- `yarn install` (for installing packages)
 - `react-scripts start` (for local launch)
 
 ## License
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
+import githubIcon from './img/GitHub-Mark-32px.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <p>
-          Work in progress! ༼ つ ◕_◕ ༽つ
+          Wiki WAM Log ༼ つ ◕_◕ ༽つ
         </p>
       </header>
+      <main className="main-content">
+        <div className="main-text">
+          WAM (Wiki Activity Monitor) — рейтинг крупнейших википроектов на FANDOM'е.
+          Каждой вики на основе различных (и неизвестных) параметров проставлялся балл от 0 до 100 (больше — лучше),
+          после чего составлялся рейтинг (который обновлялся каждый день).<br/>
+          С помощью специальных скриптов можно было сохранять у себя значения рейтинга, что и позволило построить графики.
+        </div>
+        <div className="graph">
+          <div>*Графики*</div>
+        </div>
+      </main>
+      <footer className="footer-block">
+        <div className="footer-font">
+          <a href="https://github.com/DoubleCookies" target="_blank" rel="noopener noreferrer">
+            <img src={githubIcon} width="32px" height="32px" alt={"githubIcon"} /></a>
+        </div>
+        <div className="footer-font">
+          2021 <span role="img" aria-label="cookieEmoji">🍪</span>
+        </div>
+      </footer>
     </div>
   );
 }
